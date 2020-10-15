c182552de1d316d03ffe85f0c84b9b3f1668f237
CCSH <624089195@qq.com>
2020-10-15 16:13:07 +0800

    no message

--- iOSAPP/Header(头文件)/InterfaceHeader.h
+++ iOSAPP/Header(头文件)/InterfaceHeader.h
@@ -16,10 +16,11 @@
 static NSString *request_error = @"网络异常，请检查网络";
 static NSString *success_code = @"200";
 
+#pragma mark - 三方
+#define kBuglyID @"fc42b13a1b"
+
 #pragma mark - 主机地址
 #define kHostUrl @"http://www.baidu.com/"
-
-#define kBuglyID @"fc42b13a1b"
 
 
 #pragma mark - 登录接口
